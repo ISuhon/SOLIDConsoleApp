13d48b9382a2251abb1f24d09c7cb0b57c603075
LordMushroom1 <lordmushon1@gmail.com>
2023-12-19 12:52:54 +0200

    Added class to manage linq

---
+++ SOLIDConsoleApp/MainProgram/LINQManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDConsoleApp.MainProgram
+{
+    internal class LINQManager
+    {
+    }
+}
