45575ba36baa5d5c909beefeb464cd2a4e437f75
LordMushroom1 <lordmushon1@gmail.com>
2023-12-19 12:58:04 +0200

    Added union

--- SOLIDConsoleApp/MainProgram/LINQManager.cs
+++ SOLIDConsoleApp/MainProgram/LINQManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SOLIDConsoleApp.DataBase;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,22 @@ namespace SOLIDConsoleApp.MainProgram
 {
     internal class LINQManager
     {
+        BankContext? context;
+        public LINQManager(BankContext? context) 
+        {
+            this.context = context;
+        }
+        internal void Union()
+        {
+            var ClientQuery = context?.Clients.Select(c => c.LastName);
+            var BalanceQuery = context?.Balances.Select(b => b.Surname);
+
+            var UnionQuery = ClientQuery.Union(BalanceQuery);
+
+            foreach(var union in UnionQuery) 
+            {
+                Console.WriteLine(union);
+            }
+        }
     }
 }
